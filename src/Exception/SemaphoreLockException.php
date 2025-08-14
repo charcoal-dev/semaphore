1306ffb956e73446f5079125da4ba8396262830e
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-14 22:01:22 +0500

    simplify exception header comments

--- src/Exception/SemaphoreLockException.php
+++ src/Exception/SemaphoreLockException.php
@@ -1,13 +1,7 @@
 <?php
-/*
- * This file is a part of "charcoal-dev/semaphore" package.
- * https://github.com/charcoal-dev/semaphore
- *
- * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code or visit following link:
- * https://github.com/charcoal-dev/semaphore/blob/main/LICENSE
+/**
+ * Part of the "charcoal-dev/semaphore" package.
+ * @link https://github.com/charcoal-dev/semaphore
  */
 
 declare(strict_types=1);
@@ -20,11 +14,6 @@ namespace Charcoal\Semaphore\Exception;
  */
 class SemaphoreLockException extends SemaphoreException
 {
-    /**
-     * @param \Charcoal\Semaphore\Exception\SemaphoreLockError $error
-     * @param string $message
-     * @param \Throwable|null $previous
-     */
     public function __construct(
         public readonly SemaphoreLockError $error,
         string                             $message = "",
