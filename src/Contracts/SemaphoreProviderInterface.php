dfcfb90e5f9bb62f50e56a3ab127bfb1c77f60cc
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-12-23 14:25:32 +0400

    Add `@throws SemaphoreLockException` to `SemaphoreProviderInterface::obtainLock()` docblock.

--- src/Contracts/SemaphoreProviderInterface.php
+++ src/Contracts/SemaphoreProviderInterface.php
@@ -8,11 +8,16 @@ declare(strict_types=1);
 
 namespace Charcoal\Semaphore\Contracts;
 
+use Charcoal\Semaphore\Exceptions\SemaphoreLockException;
+
 /**
  * Interface describing the contract for managing semaphore locks.
  */
 interface SemaphoreProviderInterface
 {
+    /**
+     * @throws SemaphoreLockException
+     */
     public function obtainLock(
         string  $lockId,
         ?float  $concurrentCheckEvery = null,
